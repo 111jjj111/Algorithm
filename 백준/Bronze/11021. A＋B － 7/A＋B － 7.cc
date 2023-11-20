f5207e5d0c54d08c4d976163be70ad6999dad38b
blue_sea <95969488+111jjj111@users.noreply.github.com>
2023-11-20 20:06:25 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testCase, a, b;
+	cin >> testCase;
+	for (int i = 1; i <= testCase; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << endl;
+	}
+	return 0;
+}
