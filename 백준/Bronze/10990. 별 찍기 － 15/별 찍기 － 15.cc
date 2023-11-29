78a6dabf10412c311172dbf53577f1f72369a728
KimCheongHae <95969488+111jjj111@users.noreply.github.com>
2023-11-30 03:56:18 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i = 1; i < n + 1; i++){
+        if(n == 1){
+            cout << "*";
+            break;
+        }
+        if(i > 0){
+            for(int j = n; j > i; j--){
+                cout << " ";
+            }
+            if(i > 0){
+                cout << "*";
+            }
+            for(int j = 1; j < 2 * (i - 1); j++){
+                cout << " ";
+            }
+            if(i > 1){
+                cout << "*";
+            }
+            cout << "\n";
+        }
+        
+    }
+    return 0;
+}
