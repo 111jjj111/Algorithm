d5e8deae9b69f6731cfa113a99ed44886ec304b9
KimCheongHae <95969488+111jjj111@users.noreply.github.com>
2023-12-28 23:50:50 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int a,b,c,d,e,f; cin >> a >> b >> c >> d >> e >> f;
+    int x,y;
+    for(int i = -999; i <= 999; i++) {
+        for(int j = -999; j <= 999; j++) {
+            if(a * i + b * j == c && (d * i) + (e * j) == f) {
+                cout << i << " " << j;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
